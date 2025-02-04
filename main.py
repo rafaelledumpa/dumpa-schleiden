68b9e0ae7981efc7eecedb117855171f0426f15c
rafaelledumpa <dumpa.rafaelle.schleiden@gmail.com>
2025-02-04 20:45:42 +0800

    main.py

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from turtle import *
+import turtle
 
-speed(1.2)
-bgcolor('black')
-penup()
-goto(-50,60)
-pendown()
-color('cyan')
-begin_fill()
-goto (100,100)
-goto (100,-100)
+t = turtle.Turtle()
+turtle.bgcolor("black")
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-#Draw the windows logo
-goto(-50,-60)
-goto(-50,60)
-end_fill()
-color('black')
-goto(15,100)
+#Draw Rainbow Star
+for x in range(50):
+    t.pencolor(colors[x % 7])
+    t.forward(x * 10)
+    t.right(144)
 
-#cut into 2 equal parts
-color('black')
-width(7)
-goto (15,-100)
-penup()
-goto(100,0)
-pendown()
-goto(-100,0)
-
-done()
+turtle.done()
