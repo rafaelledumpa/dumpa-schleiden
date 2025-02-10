2fbaeac9042051fee9bafb0e48174bb6a94ac868
rafaelledumpa <dumpa.rafaelle.schleiden@gmail.com>
2025-02-10 13:44:42 +0800

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 
+#Colors and Preferences 
 t = turtle.Turtle()
 turtle.bgcolor("black")
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
@@ -9,5 +10,8 @@ for x in range(50):
     t.pencolor(colors[x % 7])
     t.forward(x * 10)
     t.right(144)
+    t.speed(6.9)
 
 turtle.done()
+
+#Rafa Skibidi Toilet (Turtle Project Done)
